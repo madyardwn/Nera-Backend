7a37b0d311c627ba97471b9545a63e40ba82bae3
madyardwn <madyardwn@gmail.com>
2023-02-20 13:20:57 +0700

    Initial

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,34 +12,17 @@
 </head>
 
 <body>
-    <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-        <div class="sidebar-brand d-none d-md-flex">
-            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-                <use xlink:href="{{ asset('icons/brand.svg#full') }}"></use>
-            </svg>
-            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-                <use xlink:href="{{ asset('icons/brand.svg#signet') }}"></use>
-            </svg>
-        </div>
+    <div class="sidebar sidebar-dark sidebar-fixed bg-primary" id="sidebar">
         @include('layouts.navigation')
-        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
     </div>
     <div class="wrapper d-flex flex-column min-vh-100 bg-light">
         <header class="header header-sticky mb-4">
             <div class="container-fluid">
-                <button class="header-toggler px-md-0 me-md-3" type="button" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
-                    <svg class="icon icon-lg">
-                        <use xlink:href="{{ asset('icons/coreui.svg#cil-menu') }}"></use>
-                    </svg>
-                </button>
                 <a class="header-brand d-md-none" href="#">
                     <svg width="118" height="46" alt="CoreUI Logo">
                         <use xlink:href="{{ asset('icons/brand.svg#full') }}"></use>
                     </svg>
                 </a>
-                <ul class="header-nav d-none d-md-flex">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Dashboard</a></li>
-                </ul>
                 <ul class="header-nav ms-auto">
 
                 </ul>
